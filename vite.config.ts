3da8b92afb36b865edc307345492afc216047b1f
bparker-github <puck2361@gmail.com>
2023-08-26 17:02:49 -0400

    Add SVG loader.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueSvg from 'vite-svg-loader';
 import postcss from './postcss.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueSvg()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

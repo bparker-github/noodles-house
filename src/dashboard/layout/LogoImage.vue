b9a5d438fc164cb1b10b1c2748ef8537b60d86bd
bparker-github <puck2361@gmail.com>
2023-08-27 17:25:27 -0400

    Props required. Cleanup styles a bit.

--- src/dashboard/layout/LogoImage.vue
+++ src/dashboard/layout/LogoImage.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="logo-image flex items-center justify-center">
-    <NoodleIcon />
+  <div class="logo-image flex flex-1 items-center justify-center">
+    <NoodleIcon class="h-16 w-16 duo-gray-pink duo2-bourbon-100" />
 
-    <span class="right-text font-bold">{{ title }}</span>
+    <div class="right-text ml-2 flex w-full flex-col align-middle text-whiteish">
+      <span class="ml-10p text-2xl font-semibold leading-5">{{ title1 }}</span>
+      <span class="mr-10p text-end text-xl leading-5">{{ title2 }}</span>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+interface LogoImageProps {
+  titleRow1: string;
+  titleRow2?: string;
+}
+</script>
 
 <script setup lang="ts">
 import { NoodleIcon } from '@/core';
 import { toRef } from 'vue';
 
-const props = defineProps({ title: { type: String, default: `Noodle's House` } });
-const title = toRef(props, 'title');
+const props = defineProps<LogoImageProps>();
+const title1 = toRef(props, 'titleRow1');
+const title2 = toRef(props, 'titleRow2');
 </script>

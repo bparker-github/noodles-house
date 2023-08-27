723035d516d613e9854693ff782238bbe394cfd2
bparker-github <puck2361@gmail.com>
2023-08-27 16:27:09 -0400

    Add base icon to share.

---
+++ src/dashboard/layout/LogoImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo-image flex items-center justify-center">
+    <NoodleIcon />
+
+    <span class="right-text font-bold">{{ title }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NoodleIcon } from '@/core';
+import { toRef } from 'vue';
+
+const props = defineProps({ title: { type: String, default: `Noodle's House` } });
+const title = toRef(props, 'title');
+</script>
--- src/dashboard/layout/Sidebar.Mobile.vue
+++ src/dashboard/layout/Sidebar.Mobile.vue
@@ -2,14 +2,10 @@
 <template>
   <MobileSidebar v-model:isOpen="sidebarOpen">
     <div
-      class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10"
+      class="ring-white/10 flex grow flex-col gap-y-5 overflow-y-auto bg-mallard px-6 pb-2 ring-1"
     >
       <div class="flex h-16 shrink-0 items-center">
-        <img
-          class="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-          alt="Your Company"
-        />
+        <LogoImage class="duo-whiteish duo2-dark-orange h-8 w-auto" />
       </div>
 
       <SidebarMainNav />
@@ -22,6 +18,7 @@ import { useDashboardSidebarStore } from '@/dashboard/stores/dashboardSidebar';
 import SidebarMainNav from './Sidebar.MainNav.vue';
 import { MobileSidebar } from '@/core';
 import { storeToRefs } from 'pinia';
+import LogoImage from './LogoImage.vue';
 
 const sidebarStore = useDashboardSidebarStore();
 const { sidebarOpen } = storeToRefs(sidebarStore);

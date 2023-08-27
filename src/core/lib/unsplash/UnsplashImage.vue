24c22e94cfe3990e88083b1e0c4becc731e17cb0
bparker-github <puck2361@gmail.com>
2023-08-27 16:12:08 -0400

    Add unsplash store and component for shared use.

---
+++ src/core/lib/unsplash/UnsplashImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <img
+    :src="imageUrl"
+    alt="Gay"
+    class="h-full w-full object-cover object-center"
+  />
+</template>
+
+<script lang="ts">
+export interface UnsplashImageProps {
+  photoId: string;
+}
+</script>
+
+<script setup lang="ts">
+import { ref, toRef } from 'vue';
+import { useUnsplash } from './useUnsplash';
+import { onMounted } from 'vue';
+
+const unsplashApi = useUnsplash();
+const props = defineProps<UnsplashImageProps>();
+const propPhotoId = toRef(props, 'photoId');
+
+const imageUrl = ref('');
+
+onMounted(() => {
+  unsplashApi
+    .getPhoto(propPhotoId.value)
+    .then((resp) => (imageUrl.value = resp?.urls.full ?? imageUrl.value));
+});
+</script>

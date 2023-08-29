5badecf08fc985191159c52605a3e8da591c0521
bparker-github <puck2361@gmail.com>
2023-08-29 14:07:27 -0400

    Create authentication guard to redirect away from pages that require atuh.

---
+++ src/core/auth/AuthGuardRedirect.ts
@@ -0,0 +1,21 @@
+import type { NavigationGuard } from 'vue-router';
+import { useAuthStore } from '..';
+import { toRef } from 'vue';
+
+export const AuthGuardRedirect: NavigationGuard = async function (to) {
+  // If no auth, continue successfully.
+  if (!to.meta.authRequired) {
+    return true;
+  }
+
+  const authStore = useAuthStore();
+  const isAuthenticated = toRef(authStore, 'isAuthenticated');
+
+  // If they are successfully authenticated already, continue successfully.
+  if (isAuthenticated.value) {
+    return true;
+  }
+
+  // Otherwise redirect them to the desired route.
+  return typeof to.meta.authRequired === 'boolean' ? '/' : to.meta.authRequired;
+};

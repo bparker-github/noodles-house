c8e4d73ea246c0bca6f85e44dcffc15ce6a0a718
bparker-github <puck2361@gmail.com>
2023-08-29 13:58:02 -0400

    Profile page language, and failed page.

--- src/dashboard/pages/ProfilePage.vue
+++ src/dashboard/pages/ProfilePage.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="overflow-hidden bg-white shadow sm:rounded-lg">
     <div class="px-4 py-6 sm:px-6">
-      <h3 class="text-base font-semibold leading-7 text-gray-900">Applicant Information</h3>
-      <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+      <h3 class="text-gray-900 text-base font-semibold leading-7">Applicant Information</h3>
+      <p class="mt-1 text-gray-500 max-w-2xl text-sm leading-6">
         Personal details and application.
       </p>
     </div>
-    <div class="border-t border-gray-100">
-      <dl class="divide-y divide-gray-100">
+    <div class="border-gray-100 border-t">
+      <dl class="divide-gray-100 divide-y">
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">Full name</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+          <dt class="text-gray-900 text-sm font-medium">Full name</dt>
+          <dd class="mt-1 text-gray-700 sm:mt-0 text-sm leading-6 sm:col-span-2">Margot Foster</dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">Application for</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+          <dt class="text-gray-900 text-sm font-medium">Application for</dt>
+          <dd class="mt-1 text-gray-700 sm:mt-0 text-sm leading-6 sm:col-span-2">
             Backend Developer
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">Email address</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+          <dt class="text-gray-900 text-sm font-medium">Email address</dt>
+          <dd class="mt-1 text-gray-700 sm:mt-0 text-sm leading-6 sm:col-span-2">
             margotfoster@example.com
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">Salary expectation</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+          <dt class="text-gray-900 text-sm font-medium">Salary expectation</dt>
+          <dd class="mt-1 text-gray-700 sm:mt-0 text-sm leading-6 sm:col-span-2">$120,000</dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">About</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+          <dt class="text-gray-900 text-sm font-medium">About</dt>
+          <dd class="mt-1 text-gray-700 sm:mt-0 text-sm leading-6 sm:col-span-2">
             Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa
             consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit
             nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing
@@ -38,27 +38,27 @@
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-          <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+          <dt class="text-gray-900 text-sm font-medium leading-6">Attachments</dt>
+          <dd class="mt-2 text-gray-900 sm:mt-0 text-sm sm:col-span-2">
             <ul
               role="list"
-              class="divide-y divide-gray-100 rounded-md border border-gray-200"
+              class="divide-gray-100 border-gray-200 divide-y rounded-md border"
             >
               <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon
-                    class="h-5 w-5 flex-shrink-0 text-gray-400"
+                    class="text-gray-400 h-5 w-5 flex-shrink-0"
                     aria-hidden="true"
                   />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
                     <span class="truncate font-medium">resume_back_end_developer.pdf</span>
-                    <span class="flex-shrink-0 text-gray-400">2.4mb</span>
+                    <span class="text-gray-400 flex-shrink-0">2.4mb</span>
                   </div>
                 </div>
                 <div class="ml-4 flex-shrink-0">
                   <a
                     href="#"
-                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                    class="text-indigo-600 hover:text-indigo-500 font-medium"
                     >Download</a
                   >
                 </div>
@@ -66,18 +66,18 @@
               <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon
-                    class="h-5 w-5 flex-shrink-0 text-gray-400"
+                    class="text-gray-400 h-5 w-5 flex-shrink-0"
                     aria-hidden="true"
                   />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
                     <span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                    <span class="flex-shrink-0 text-gray-400">4.5mb</span>
+                    <span class="text-gray-400 flex-shrink-0">4.5mb</span>
                   </div>
                 </div>
                 <div class="ml-4 flex-shrink-0">
                   <a
                     href="#"
-                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                    class="text-indigo-600 hover:text-indigo-500 font-medium"
                     >Download</a
                   >
                 </div>
@@ -90,6 +90,6 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { PaperClipIcon } from '@heroicons/vue/20/solid';
 </script>

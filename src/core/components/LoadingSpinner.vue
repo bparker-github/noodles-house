9cb0a46e402262ef41fa7fe0a39dfbdd15119269
bparker-github <puck2361@gmail.com>
2023-09-03 12:10:03 -0400

    Update spinner styles.

--- src/core/components/LoadingSpinner.vue
+++ src/core/components/LoadingSpinner.vue
@@ -5,7 +5,7 @@
   >
     <svg
       aria-hidden="true"
-      class="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+      class="mr-2 h-8 w-8 animate-spin fill-bourbon-700 text-bourbon-900 dark:text-bourbon-300"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"

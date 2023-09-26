2202fbf326aa8fe6ab65813a6b3863ed71f8b2ec
bparker-github <puck2361@gmail.com>
2023-09-26 11:14:56 -0400

    Move the modules, but also include the package updates.

--- modules/shared/src/integrations/unsplash/unsplash.d.ts
+++ modules/shared/src/integrations/unsplash/unsplash.d.ts
@@ -1,3 +1,32 @@
 import { createApi } from 'unsplash-js';
 
+interface BaseUnsplashPhotoRequest {
+  /** The identifier of the photo itself. */
+  id: string;
+
+  /** The value to pass as the width query parameter. */
+  qp_width?: string;
+  /** The value to pass as the height query parameter. */
+  qp_height?: string;
+  /**
+   * The fit mode to use as the query parameter.
+   * @default 'clip'
+   */
+  qp_fit?: 'clamp' | 'clip' | 'crop' | 'facearea' | 'fill' | 'fillmax' | 'max' | 'min' | 'scale';
+}
+interface CropRequest extends BaseUnsplashPhotoRequest {
+  /** Required width, height, and crop fit. */
+  qp_height: string;
+  qp_width: string;
+  qp_fit: 'crop';
+}
+
+export interface UnsplashPhotoRequest {
+  id: string;
+  qp_width: string;
+  qp_height: string;
+  qp_crop: string;
+}
+// always add "auto=fit"
+
 export type UnsplashApi = ReturnType<typeof createApi>;
--- modules/ui/tailwind.config.ts
+++ modules/ui/tailwind.config.ts
@@ -2,7 +2,12 @@ import { applyThemeToTailwindConfig } from './src/plugins/TailwindConfigAdapter'
 import type { Config } from 'tailwindcss';
 
 const LocalConfig: Config = {
-  content: ['./index.html', './src/components/**/*', './src/dashboard/**/*'],
+  content: [
+    './index.html',
+    './src/components/**/*',
+    './src/dashboard/**/*',
+    '../blur-hash/src/**/*',
+  ],
 };
 
 export default applyThemeToTailwindConfig(LocalConfig);

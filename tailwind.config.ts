ded3e4243d5e2fc2cc88905e54a266af6af70c88
bparker-github <puck2361@gmail.com>
2023-09-26 12:30:29 -0400

    Update base config files.

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import { generateConfigForContent } from './code/modules/tailwind';
+export default generateConfigForContent([
+  './code/ui/index.html',
+  './code/ui/src/**/*',
+  './code/modules/blur-hash/src/**/*',
+  './code/modules/blur-hash/src/**/*',
+]);

4f06379f06ff3b0700a5b5575fe81fd95959e034
bparker-github <puck2361@gmail.com>
2023-09-26 12:30:15 -0400

    Delete old fork

--- forks/blur-hash/src/tools.ts
+++
@@ -1,8 +0,0 @@
-export function parseAsInt(num: number | string) {
-  return typeof num === 'number' ? num : parseInt(num, 10);
-}
-
-export function percentAsRatio(height: number, width: number) {
-  const val = (Math.round(height) / Math.round(width)) * 100;
-  return val.toFixed(2) + '%';
-}

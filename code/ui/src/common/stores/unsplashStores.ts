2d732187701e59c2f93e94ec2c20ca84e1bb5088
bparker-github <puck2361@gmail.com>
2023-10-07 12:14:33 -0400

    Update store to properly handle primary and secondary apis

--- code/ui/src/common/stores/unsplashStores.ts
+++ code/ui/src/common/stores/unsplashStores.ts
@@ -1,5 +1,6 @@
 import { doGetOrThrow, useUnsplashApi, type UnsplashApi } from '@noodles-house/common';
 import { defineStore } from 'pinia';
+import type { ApiResponse } from 'unsplash-js/dist/helpers/response';
 import type { Full } from 'unsplash-js/dist/methods/photos/types';
 import { ref } from 'vue';
 
@@ -25,26 +26,51 @@ export const useUnsplash = defineStore('unsplash', () => {
   const unsplashApi = ref(useUnsplashApi('ui', unsplashUrl));
   const backupApi = ref(useUnsplashApi('api', unsplashAccessKey));
 
-  async function handlePhotoResponse(api: UnsplashApi, photoId: string): Promise<Full | null> {
-    try {
-      const resp = await api.photos.get({ photoId });
+  // Keep a cache of these images.
+  const cache = ref<Record<string, Full>>({});
+  const didLocalApiFail = ref(false);
 
-      if (!resp || resp.type === 'error') {
-        console.error('Failed fetching data from Unsplash:', resp.errors);
-        return null;
-      }
-      return resp.response;
-    } catch (err) {
-      console.error('Error fetching data from our UnsplashApi:', err);
-      return null;
-    }
-  }
+  const getDataFromCache = (id: string): Full | null =>
+    id in cache.value ? cache.value[id] : null;
+  const getDataFromApi = (api: UnsplashApi, id: string): Promise<ApiResponse<Full>> =>
+    api.photos
+      .get({ photoId: id })
+      // @ts-expect-error - it doesn't fulfill all properties, but we only utilize these two.
+      .catch((e) => ({ type: 'error', errors: [e] }) as ApiResponse<Full>);
+
   async function getPhoto(id: string = 'W7cPLHOa0eQ'): Promise<Full | null> {
-    let resp = await handlePhotoResponse(unsplashApi.value, id);
-    if (!resp && unsplashAccessKey && backupApi.value) {
-      resp = await handlePhotoResponse(backupApi.value, id);
+    try {
+      // Step one, return if data is in cache already.
+      const cached = getDataFromCache(id);
+      if (cached) {
+        return cached;
+      }
+
+      // Make a request to the primary API, iff we haven't deemed it useless.
+      if (!didLocalApiFail.value) {
+        const primaryResp = await getDataFromApi(unsplashApi.value, id);
+        if (primaryResp.type !== 'error') {
+          cache.value[id] ??= primaryResp.response;
+          return primaryResp.response;
+        }
+        console.error(`Unsplash API Error: ${primaryResp.errors}`);
+      }
+
+      // Make a request to the backup API, iff we are allowed and able to.
+      if (import.meta.env.DEV && unsplashAccessKey && backupApi.value) {
+        const backupResp = await getDataFromApi(backupApi.value, id);
+        if (backupResp.type !== 'error') {
+          cache.value[id] ??= backupResp.response;
+          return backupResp.response;
+        }
+        console.error(`Unsplash Backup API Error: ${backupResp.errors}`);
+      }
+
+      return null;
+    } catch (err) {
+      console.error('Unsplash image fetching error:');
+      throw err;
     }
-    return resp;
   }
 
   return {

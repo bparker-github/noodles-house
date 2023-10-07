4a1eb3c8ab666404dd7454293b5769512611944a
bparker-github <puck2361@gmail.com>
2023-10-07 13:58:16 -0400

    Update credit to use the new components.

--- code/ui/src/common/components/UnsplashImageCredit.vue
+++ code/ui/src/common/components/UnsplashImageCredit.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- We must credit the creator of the images with a click section -->
-  <RouterLink
+  <NoodleLink
     v-if="photoResp"
     :class="[
       'credit-section absolute bottom-0 left-0 right-0 px-3 py-1',
@@ -9,20 +9,22 @@
     :to="createAttributeLink(photoResp.links.html)"
   >
     <span class="pr-1">Credit:</span>
-    <RouterLink
+    <NoodleLink
       :to="createAttributeLink(photoResp.user.portfolio_url!)"
       class="italic"
-      >{{ photoResp.user.name }}</RouterLink
+      >{{ photoResp.user.name }}</NoodleLink
     >
     <span class="px-1">on</span>
-    <RouterLink :to="createAttributeLink('https://unsplash.com/')">Unsplash</RouterLink>
-  </RouterLink>
+    <NoodleLink :to="createAttributeLink('https://unsplash.com/')">Unsplash</NoodleLink>
+  </NoodleLink>
 </template>
 
 <script setup lang="ts">
 import type { Full } from 'unsplash-js/dist/methods/photos/types';
+import NoodleLink from './NoodleLink.vue';
 
-defineProps<{ photoResp: Full | null }>();
+const props = defineProps<{ photoResp: Full | null }>();
+console.log('Credit Props:', props);
 
 function createAttributeLink(link: string): string {
   const separator = link.match(/\?w=/i) ? '&' : '?';

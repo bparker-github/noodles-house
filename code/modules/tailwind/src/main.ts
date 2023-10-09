aad33303de014b8302037b655440e25f5ca5c944
bparker-github <puck2361@gmail.com>
2023-10-09 10:45:57 -0400

    This is so irritating. Just keep it all in one file for now - we only have one entrypoint in the UI.

--- code/modules/tailwind/src/main.ts
+++
@@ -1,3 +0,0 @@
-export { generateConfigForContent } from './generateConfig';
-export { applyThemeToTailwindConfig } from './plugins/TailwindConfigAdapter';
-export { TailwindDuotonePlugin } from './plugins/TailwindDuotonePlugin';

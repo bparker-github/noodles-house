b5e0b6ed8e904ab2319e8e761435521eb5ce88ea
bparker-github <puck2361@gmail.com>
2023-10-09 10:09:41 -0400

    This was moved

--- code/ui/src/tailwind/generateConfig.ts
+++
@@ -1,6 +0,0 @@
-import { type Config } from 'tailwindcss';
-import { applyThemeToTailwindConfig } from './plugins/TailwindConfigAdapter';
-
-export function generateConfigForContent(content: string[]): Config {
-  return applyThemeToTailwindConfig({ content });
-}

66a53a1a4df8d722c99535c3090c3b8feaa060dc
bparker-github <puck2361@gmail.com>
2023-10-14 12:02:52 -0400

    No need for this helper any longer.

--- code/ui/src/common/auth/index.ts
+++ code/ui/src/common/auth/index.ts
@@ -1,13 +1,2 @@
-import type { AuthStoreShape } from '@noodles-house/common';
-import { toValue } from 'vue';
-import { type Router } from 'vue-router';
-import { CustomNavigationClient } from './NavigationGuard';
-
 export { AuthGuardRedirect } from './AuthGuardRedirect';
 export { CustomNavigationClient } from './NavigationGuard';
-
-export function applyNavigationRouter(router: Router, app: AuthStoreShape) {
-  // This is how you tell MSAL how to route using our vue router.
-  const navClient = new CustomNavigationClient(router);
-  toValue(app.instance).setNavigationClient(navClient);
-}

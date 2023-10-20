ceb42e961824c67c5624c8edf9049ece92bcbc6d
bparker-github <puck2361@gmail.com>
2023-10-20 17:55:37 -0400

    Add in roles and permissions

---
+++ code/modules/common/src/auth/authorization/Permissions.ts
@@ -0,0 +1,46 @@
+export enum PermissionType {
+  VIEW_DASHBOARD = 'View Dashboard',
+  VIEW_ADMIN_PANEL = 'View Admin Panel',
+}
+
+export interface Permission {
+  type: PermissionType;
+  description: string;
+  forGroups: string[];
+}
+
+const ALL_PERMISSIONS: Permission[] = [
+  {
+    type: PermissionType.VIEW_ADMIN_PANEL,
+    description: 'This user may view the Admin page and data related.',
+    forGroups: [],
+  },
+  {
+    type: PermissionType.VIEW_DASHBOARD,
+    description: 'This user may view the Dashboard data',
+    forGroups: [],
+  },
+];
+
+export function getAllPermissionTypes(): PermissionType[] {
+  return ALL_PERMISSIONS.map((p) => p.type);
+}
+export function getAllPermissions(): Permission[] {
+  return [...ALL_PERMISSIONS];
+}
+export function getPermissionFrom(...types: PermissionType[]): Permission[] {
+  return types.filter((type) => type in ALL_PERMISSIONS).map((type) => ALL_PERMISSIONS[type]);
+}
+export function getPermissionsFromGroups(...groups: string[]): PermissionType[] {
+  const foundPerms = new Set<PermissionType>();
+
+  // Check all given groups.
+  for (const group of groups) {
+    // Filter permissions to ones that match this group.
+    const permsForGroups = ALL_PERMISSIONS.filter((p) => p.forGroups.includes(group));
+    // Add each of these roles to the set - de-duped automatically.
+    permsForGroups.forEach((p) => foundPerms.add(p.type));
+  }
+
+  return Array.from(foundPerms);
+}

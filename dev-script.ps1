23c20832f66db38d99e71ffcd51ae7f530a1ed28
bparker-github <puck2361@gmail.com>
2023-11-18 16:52:06 -0500

    Add dev script and powershell to project for starting tabs easier.

---
+++ dev-script.ps1
@@ -0,0 +1,24 @@
+# Note: The title changes based on the commands running too - labeling the tabs is hard.
+
+function startUi {
+  # Create the UI tab
+  wt -w 0 nt --title UI --inheritEnvironment -d . pwsh -NoExit -c "npm run dev:ui"
+  Write-Output "Creating UI Tab..."
+  Start-Sleep -Seconds 5
+
+  # Create teh API tab
+  wt -w 0 nt --title API --inheritEnvironment -d . pwsh -NoExit -c "npm run dev:api"
+  Write-Output "Creating API Tab..."
+  Start-Sleep -Seconds 5
+
+  # Start the SWA tab.
+  $tabSWA = wt -w 0 nt --title SWA --inheritEnvironment -d . pwsh -NoExit  -c "npm run dev:swa:debug"
+  Write-Output "New Tabs have been created... Press enter to continue"
+  Read-Host
+  $tabSWA.Focus;
+  
+  Exit-PSSession 0;
+}
+
+# Run on reference
+startUI;

f8ff8925ff16db1319f6d66cb24b412a469c7c3e
bparker-github <puck2361@gmail.com>
2023-11-18 15:05:50 -0500

    Add stacked layout, begin creating my own version

---
+++ ui/src/layouts/overlap/HeaderRow.vue
@@ -0,0 +1,16 @@
+<template>
+  <header :class="['stacked-overlap-header-row', 'fixed flex flex-1']">
+    <!-- Left side -->
+    <div class="left-side">
+      <NoodleIconSvg />
+    </div>
+
+    <!-- Main, center, growing -->
+    <div class="main-center">Main Center</div>
+
+    <!-- Right side -->
+    <div class="right-side">Right side.</div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>

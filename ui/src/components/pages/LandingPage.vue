7b79e12e6aa473a73b9367e3e49d970736395d2e
bparker-github <puck2361@gmail.com>
2023-11-22 18:41:51 -0500

    Update to stand alone without layout

--- ui/src/components/pages/LandingPage.vue
+++ ui/src/components/pages/LandingPage.vue
@@ -1,12 +1,27 @@
 import { UnsplashImage } from '@/lib';
 <template>
-  <div class="landing-page flex flex-col flex-1 w-full">
+  <div class="landing-page flex flex-col flex-1 w-full h-full">
     <UnsplashImage
       id="W7cPLHOa0eQ"
       class="shadow-lg flex flex-col flex-1 m-3"
-      shared-class="rounded-md"
+      shared-class="flex flex-col flex-1 items-center justify-center rounded-md"
+      credit-class="text-nh-whiteish rounded-b-md"
     >
-      <div>Behold, it's me - the problem.</div>
+      <div
+        :class="[
+          'landing-page-image-content',
+          'flex flex-col',
+          'prose p-10 text-nh-whiteish bg-nh-bali-hai-500/70',
+        ]"
+      >
+        <h1>Welcome</h1>
+        <h2>Noodle's House</h2>
+
+        <p>This is a landing page for the future Noodle's House page.</p>
+        <p>Pardon the construction</p>
+      </div>
+
+      <button />
     </UnsplashImage>
   </div>
 </template>
@@ -14,3 +29,9 @@ import { UnsplashImage } from '@/lib';
 <script setup lang="ts">
 import { UnsplashImage } from '@/lib';
 </script>
+
+<style>
+#app {
+  @apply bg-nh-bourbon-100;
+}
+</style>

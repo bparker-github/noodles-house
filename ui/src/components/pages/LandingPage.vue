0eb9644eb2ca683ca7c85c3d3d6b9593788c4591
bparker-github <puck2361@gmail.com>
2023-11-22 18:27:09 -0500

    Update landing page

--- ui/src/components/pages/LandingPage.vue
+++ ui/src/components/pages/LandingPage.vue
@@ -1,9 +1,10 @@
 import { UnsplashImage } from '@/lib';
 <template>
-  <div class="landing-page flex flex-col flex-1 w-full p-2 m-3">
+  <div class="landing-page flex flex-col flex-1 w-full">
     <UnsplashImage
       id="W7cPLHOa0eQ"
-      class="h-90p w-90p rounded-md shadow-lg"
+      class="shadow-lg flex flex-col flex-1 m-3"
+      shared-class="rounded-md"
     >
       <div>Behold, it's me - the problem.</div>
     </UnsplashImage>

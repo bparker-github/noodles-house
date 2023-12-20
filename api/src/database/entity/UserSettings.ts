3533fa99e2533f9c677877c946d69ca08cbbb22f
bparker-github <puck2361@gmail.com>
2023-12-20 11:38:21 -0500

    Update the name of the table.

--- api/src/database/entity/UserSettings.ts
+++ api/src/database/entity/UserSettings.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity('UserSettings')
 export class UserSettings {
   @PrimaryGeneratedColumn('uuid')
   id: string;

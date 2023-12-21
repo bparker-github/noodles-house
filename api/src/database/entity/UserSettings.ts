403a16d0c795a54c75df6421ee136cb990bdf795
bparker-github <puck2361@gmail.com>
2023-12-21 10:20:58 -0500

    Add back the primary column for user Settings

--- api/src/database/entity/UserSettings.ts
+++ api/src/database/entity/UserSettings.ts
@@ -1,8 +1,11 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('UserSettings')
 export class UserSettings {
-  @PrimaryColumn('varchar', { length: 255 })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar', { length: 255, name: 'userId' })
   userId: string;
 
   @Column()
@@ -16,6 +19,7 @@ export class UserSettings {
 
   static Default(userId = ''): UserSettings {
     return {
+      id: '',
       userId,
       firstName: '',
       lastName: '',

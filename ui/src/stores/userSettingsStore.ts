8d4b5d9c267ee81d4147e9966e0d48dadd8283ab
bparker-github <puck2361@gmail.com>
2023-12-21 10:43:59 -0500

    Simplify the user settings store.

--- ui/src/stores/userSettingsStore.ts
+++ ui/src/stores/userSettingsStore.ts
@@ -1,8 +1,8 @@
-import { useNativeAuth } from '@/auth/useNativeAuth';
-import { useFetch } from '@vueuse/core';
-import { defineStore, storeToRefs } from 'pinia';
+import { useFetch, useSessionStorage } from '@vueuse/core';
+import { defineStore } from 'pinia';
 import { computed } from 'vue';
 
+/** Required headers for the API /data-api */
 const DEFAULT_headers = {
   headers: {
     'Content-Type': 'application/json',
@@ -19,32 +19,38 @@ export interface UserSettings {
 }
 
 export const useUserSettings = defineStore('user-settings-store', () => {
-  const baseRoute = '/data-api/api/UserSettings';
-
-  const { curUser } = storeToRefs(useNativeAuth());
-
-  const getUserSettingsUrl = computed(
-    () => baseRoute + '/userId/' + curUser.value?.clientPrincipal.userId
-  );
-
-  const getAllUserSettingsFetch = useFetch<UserSettings[]>(
-    '/data-api/api/UserSettings',
-    DEFAULT_headers,
-    { immediate: false }
-  ).json<UserSettings[]>();
-
-  const getUserSettingsFetch = useFetch<UserSettings | null>(getUserSettingsUrl, DEFAULT_headers, {
+  /** FetchFunctions to handle the requests and their states */
+  const Fetch_GetMySettings = useFetch<UserSettings | null>('/api/settings', {
     immediate: false,
+    afterFetch: (ctx) => {
+      myUserSettings.value = !ctx.response.ok ? null : ctx.data;
+      return ctx;
+    },
   }).json<UserSettings | null>();
+  function fetchMySettings(force = false) {
+    if (force) {
+      myUserSettings.value = null;
+    }
+    return Fetch_GetMySettings.execute();
+  }
 
-  const curSettings = computed(() =>
-    !getUserSettingsFetch.isFinished.value ? null : getUserSettingsFetch.data.value
+  // Use local variables here to manage the longer term storage of the cur settings.
+  const myUserSettings = useSessionStorage<UserSettings | null>(
+    'noodles-house-user-settings',
+    Fetch_GetMySettings.data.value ?? null,
+    { serializer: { read: JSON.parse, write: JSON.stringify } }
   );
 
   return {
-    curSettings,
+    // Refs
+    myUserSettings,
 
-    getUserSettingsFetch,
-    getAllUserSettingsFetch,
+    // Computed
+    isFetchMySettingsFetching: computed(() => Fetch_GetMySettings.isFetching.value),
+    isFetchMySettingsFinished: computed(() => Fetch_GetMySettings.isFinished.value),
+    isFetchMySettingsError: computed(() => Fetch_GetMySettings.error.value),
+
+    // Functions
+    fetchMySettings,
   };
 });

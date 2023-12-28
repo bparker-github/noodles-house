4c38d61aa08c5ba0b87bc929d0287e8d9b6b1a09
bparker-github <puck2361@gmail.com>
2023-12-28 06:17:10 -0600

    Add first pass of UserSettings page

--- ui/src/components/pages/UserSettings.vue
+++ ui/src/components/pages/UserSettings.vue
@@ -3,17 +3,19 @@
     <NhButton
       :b-style="ButtonStyle.SOLID"
       :b-theme="DefaultTheme.BALI_HAI"
-      text="Get Settings"
-      :is-loading="isFetchMySettingsFetching"
+      :text="buttonText"
+      :is-loading="isFetchMySettingsFetching || true"
       @click="getSettings"
     />
 
-    <InfoListCard
+    <EditableSettingsList
       v-if="myUserSettings"
+      v-model:data="myUserSettings"
+      :model-labels="settingsLabels"
+      :update-model-val="updateModelVal"
       title="User Settings"
       sub-title="These settings are entirely up to your preference, m'dear."
-      :list="settingsList"
-      class="rounded-md"
+      @on-save="onSettingsSave"
     />
 
     <div v-if="myUserSettings">
@@ -25,12 +27,14 @@
 
 <script setup lang="ts">
 import { type EnumObject } from '@/lib';
-import { useUserSettings } from '@/stores/userSettingsStore';
+import { useUserSettings, type UserSettings } from '@/stores/userSettingsStore';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
 import NhButton, { ButtonStyle, DefaultTheme } from '../basic/NhButton.vue';
-import InfoListCard from '../cards/InfoListCard.vue';
 import PreCodeBlock from '../PreCodeBlock.vue';
+import EditableInfoList from '../forms/EditableInfoList.vue';
+
+const EditableSettingsList = EditableInfoList<UserSettings>;
 
 const userSettingsStore = useUserSettings();
 const { myUserSettings, isFetchMySettingsFetching } = storeToRefs(userSettingsStore);
@@ -45,19 +49,36 @@ async function getSettings() {
   }
 }
 
-const settingsList = computed<EnumObject[]>(() => {
-  if (myUserSettings.value) {
-    return Object.entries(myUserSettings.value).map(([label, value]) => ({
-      label,
-      value: value || '<none>',
-    }));
-  } else {
-    return [
-      {
-        label: 'No User Settings saved yet!',
-        value: 'null',
-      },
-    ];
+// Computed
+const buttonText = computed(() => (myUserSettings.value ? 'Refresh Settings' : 'Get Settings'));
+const settingsLabels = computed<EnumObject<keyof UserSettings>[]>(() =>
+  !myUserSettings.value
+    ? [
+        {
+          label: 'No User Settings saved yet!',
+          value: null as unknown as keyof UserSettings,
+        },
+      ]
+    : Object.entries(myUserSettings.value).map(([label, value]) => ({
+        label,
+        value: label as keyof UserSettings,
+      }))
+);
+
+function updateModelVal<K extends keyof UserSettings>(key: K, newVal: UserSettings[K]) {
+  // Short circuit if something fishy happens.
+  if (!myUserSettings.value || !(key in myUserSettings.value)) {
+    console.log('error updating model val:', key, newVal);
+    return myUserSettings.value ?? undefined;
   }
-});
+
+  myUserSettings.value = {
+    ...myUserSettings.value,
+    [key]: newVal,
+  };
+
+  return myUserSettings.value;
+}
+
+function onSettingsSave() {}
 </script>

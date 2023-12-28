8924771124058dbfb65769ea81847961d5d8b2e2
bparker-github <puck2361@gmail.com>
2023-12-28 16:08:45 -0500

    Add utility type

--- ui/src/lib/TypeAugments.d.ts
+++ ui/src/lib/TypeAugments.d.ts
@@ -2,3 +2,11 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<
   {
     [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>;
   }[Keys];
+
+export type ChangeValues<T extends {}, V> = {
+  [key in T]: T[key] extends Object
+    ? ChangeValues<T[key], V>
+    : T[key] extends Array<infer F>
+      ? Array<ChangeValues<F, V>>
+      : V;
+};

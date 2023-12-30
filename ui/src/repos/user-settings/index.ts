23d61a152e4d4da9ed520e83f196d45a05e095b7
bparker-github <puck2361@gmail.com>
2023-12-30 11:05:40 -0500

    Add repos folder to centralize possible data-api hoookups i the future

---
+++ ui/src/repos/user-settings/index.ts
@@ -0,0 +1,2 @@
+export * from './UserSettings.d';
+export * from './repository';
---
+++ ui/src/repos/user-settings/repository.ts
@@ -0,0 +1,52 @@
+import { useNativeAuth } from '@/auth/useNativeAuth';
+import { useFetch, useSessionStorage } from '@vueuse/core';
+import { defineStore, storeToRefs } from 'pinia';
+import { type UserSettings } from '.';
+import { getFetchHeaders } from '../helpers';
+
+export const userSettingsRepository = defineStore('user-settings-repo', () => {
+  // Set up the local ref for the settings
+  //  Persist to session storage for performance on repeat page-loads.
+  const myUserSettings = useSessionStorage<UserSettings | null>('user-settings-temp', null, {
+    serializer: { read: JSON.parse, write: JSON.stringify },
+  });
+
+  // Load the native-auth for the user id.
+  const { curUser } = storeToRefs(useNativeAuth());
+  const GET_fetch = useFetch<UserSettings[]>(
+    `/data-api/direct/user-settings/id/${curUser.value?.clientPrincipal.userId}`,
+    {
+      headers: getFetchHeaders('authenticated'),
+      method: 'GET',
+    },
+    { immediate: false }
+  ).json<UserSettings[]>();
+
+  async function getUserSettings(): Promise<UserSettings | null> {
+    if (!curUser.value) {
+      console.error('Cannot get user settings without the user id');
+      return null;
+    }
+
+    // Execute the fetch, update our local ref settings, return the found value.
+    try {
+      await GET_fetch.execute();
+      myUserSettings.value = GET_fetch.response.value?.ok ? GET_fetch.data.value?.[0] : null;
+      return myUserSettings.value;
+    } catch (err) {
+      console.error('Failed to get user settings:', err);
+      return null;
+    }
+  }
+
+  return {
+    // Refs
+    myUserSettings,
+
+    // Fetch Hooks
+    GET_fetch,
+
+    // Functions
+    getUserSettings,
+  };
+});

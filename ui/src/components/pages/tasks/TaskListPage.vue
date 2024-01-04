c6a27e642a919b83a222c5c369978b9f82f96877
bparker-github <puck2361@gmail.com>
2024-01-04 16:39:53 -0500

    Move todos page to /tasks pages

---
+++ ui/src/components/pages/tasks/TaskListPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="task-list-page flex flex-col gap-y-4">
+    <TaskInfoAndNavCard />
+    <TaskListCard />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TaskInfoAndNavCard from '../../tasks/TaskInfoAndNavCard.vue';
+import TaskListCard from '../../tasks/TaskListCard.vue';
+</script>

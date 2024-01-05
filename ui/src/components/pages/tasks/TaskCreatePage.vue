8256f70942d3c15765117a13998bdbec91d2b478
bparker-github <puck2361@gmail.com>
2024-01-05 10:11:33 -0500

    Update Create and List page to use the task store, and pass the related values into the card components

--- ui/src/components/pages/tasks/TaskCreatePage.vue
+++ ui/src/components/pages/tasks/TaskCreatePage.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="task-list-page flex flex-col gap-y-4">
     <TaskInfoAndNavCard />
-    <TaskCreateCard />
+    <TaskCreateCard
+      :is-submit-loading="create_isFetching"
+      @submit="submitTask"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import TaskInfoAndNavCard from '../../tasks/TaskInfoAndNavCard.vue';
 import TaskCreateCard from '../../tasks/TaskCreateCard.vue';
+import type { TodoTask } from '@db/models/TodoTask';
+import { useTaskStore } from '@/stores/tasksStore';
+import { storeToRefs } from 'pinia';
+
+const taskStore = useTaskStore();
+const { create_isFetching } = storeToRefs(taskStore);
+
+async function submitTask(task: TodoTask) {
+  try {
+    const created = await taskStore.createTask(task);
+    console.log('Created a new task:', created);
+  } catch (err) {
+    console.error('Failed to create a new task:', err);
+  }
+}
 </script>
--- ui/src/components/pages/tasks/TaskListPage.vue
+++ ui/src/components/pages/tasks/TaskListPage.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="task-list-page flex flex-col gap-y-4">
     <TaskInfoAndNavCard />
-    <TaskListCard />
+    <TaskListCard
+      :all-tasks="knownTasks"
+      :is-fetching="getAll_isFetching"
+      @refresh="doRefresh"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useTaskStore } from '@/stores/tasksStore';
 import TaskInfoAndNavCard from '../../tasks/TaskInfoAndNavCard.vue';
 import TaskListCard from '../../tasks/TaskListCard.vue';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
+
+const taskStore = useTaskStore();
+const { knownTasks, getAll_isFetching } = storeToRefs(taskStore);
+
+function doRefresh(): Promise<unknown> {
+  // Don't double-fetch.
+  if (getAll_isFetching.value) {
+    return Promise.resolve();
+  }
+
+  return taskStore.getAllTasks(true);
+}
+
+onMounted(doRefresh);
 </script>

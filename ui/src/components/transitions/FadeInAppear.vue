1cc5e9acefc00e9cdf0d78cf24cea0b9e66424ae
bparker-github <puck2361@gmail.com>
2024-01-13 13:41:01 -0500

    Add in FadeInAppear utility transition and use it for the Select field.

---
+++ ui/src/components/transitions/FadeInAppear.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition
+    :class="['motion-safe:transform transition-opacity', { 'duration-200': !hasDurationClass }]"
+    :appear="true"
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+  >
+    <slot></slot>
+  </transition>
+</template>
+
+<script setup lang="ts">
+import { computed, useAttrs } from 'vue';
+
+const attrs = useAttrs();
+
+const hasDurationClass = computed(() => {
+  const ret = /duration-/g.test(attrs?.['class'] ?? '');
+  console.log('Checking duration class:', attrs?.['class'], ret);
+  return /duration-/g.test(attrs?.['class'] ?? '');
+});
+</script>

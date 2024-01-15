7b58b58156d3409c4c0a4da4914252c86b136486
bparker-github <puck2361@gmail.com>
2024-01-15 18:21:46 -0500

    Update base app styling

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,7 +3,10 @@
     v-if="loading"
     class="initial-loader"
   />
-  <RouterView v-else />
+  <RouterView
+    v-else
+    class="relative"
+  />
 </template>
 
 <script setup lang="ts">
@@ -34,6 +37,6 @@ onMounted(async () => {
 html,
 body,
 div#app {
-  @apply h-full w-full;
+  @apply max-h-screen min-h-0 w-screen;
 }
 </style>

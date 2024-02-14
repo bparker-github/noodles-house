9cd838c50d24db561ef8ef8db377369344d8368a
bparker-github <puck2361@gmail.com>
2024-02-14 11:03:47 -0500

    Add page spinners to login/logout, clear cache on latter

--- ui/src/components/pages/auth/LoginPage.vue
+++ ui/src/components/pages/auth/LoginPage.vue
@@ -1,8 +1,12 @@
+<template>
+  <PageSpinner title="Logging out..." />
+</template>
+
 <script setup lang="ts">
+import PageSpinner from '@/components/spinners/PageSpinner.vue';
 import { onBeforeMount } from 'vue';
 
 onBeforeMount(() => {
-  console.debug('LoginPage before mount.');
   window.location.pathname = '/login';
 });
 </script>
--- ui/src/components/pages/auth/LogoutPage.vue
+++ ui/src/components/pages/auth/LogoutPage.vue
@@ -1,8 +1,16 @@
+<template>
+  <PageSpinner title="Logging out..." />
+</template>
+
 <script setup lang="ts">
+import { useNativeAuth } from '@/auth/useNativeAuth';
+import PageSpinner from '@/components/spinners/PageSpinner.vue';
 import { onBeforeMount } from 'vue';
 
+const authStore = useNativeAuth();
+
 onBeforeMount(() => {
-  console.debug('LogoutPage before mount.');
+  authStore.clearAuthCache();
   window.location.pathname = '/logout';
 });
 </script>

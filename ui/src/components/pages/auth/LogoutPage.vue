30ee2d0f4e5f56a41f9ec9679f7b66bebdc3ec7c
bparker-github <puck2361@gmail.com>
2024-02-19 13:41:11 -0500

    Clear the user settings on logout

--- ui/src/components/pages/auth/LogoutPage.vue
+++ ui/src/components/pages/auth/LogoutPage.vue
@@ -5,12 +5,16 @@
 <script setup lang="ts">
 import { useNativeAuth } from '@/auth/useNativeAuth';
 import PageSpinner from '@/components/spinners/PageSpinner.vue';
+import { userSettingsRepository } from '@/repos/user-settings';
 import { onBeforeMount } from 'vue';
 
 const authStore = useNativeAuth();
+const userSettings = userSettingsRepository();
 
 onBeforeMount(() => {
   authStore.clearAuthCache();
+  userSettings.clearSettingsCache();
+
   window.location.pathname = '/logout';
 });
 </script>

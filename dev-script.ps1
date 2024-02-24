82d55cbe2bcc04817f15f489700c3d3ccbc1df6d
bparker-github <puck2361@gmail.com>
2024-02-24 11:55:03 -0500

    Remove sleep from script

--- dev-script.ps1
+++ dev-script.ps1
@@ -10,12 +10,12 @@ function startUi {
   # Create the UI tab
   wt -w 0 nt --title UI --inheritEnvironment -d . pwsh -NoExit -c "npm run dev:ui"
   Write-Output "Creating UI Tab..."
-  Start-Sleep -Seconds 2
+  # Start-Sleep -Seconds 2
 
   # Create teh API tab
   wt -w 0 nt --title API --inheritEnvironment -d . pwsh -NoExit -c "npm run dev:api"
   Write-Output "Creating API Tab..."
-  Start-Sleep -Seconds 5
+  # Start-Sleep -Seconds 5
 
   # Start the SWA tab.
   $tabSWA = wt -w 0 nt --title SWA --inheritEnvironment -d . pwsh -NoExit  -c "npm run dev:swa:debug"

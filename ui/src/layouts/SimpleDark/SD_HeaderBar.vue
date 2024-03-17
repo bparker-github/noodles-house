a982b95d7918a7205e1a71a591fa0dad95b216f5
bparker-github <puck2361@gmail.com>
2024-03-17 13:53:02 -0400

    Add tailwind support for custom drop-shadow colors

--- ui/src/layouts/SimpleDark/SD_HeaderBar.vue
+++ ui/src/layouts/SimpleDark/SD_HeaderBar.vue
@@ -6,123 +6,47 @@
       'bg-gradient-to-b from-nh-chalet-green-400 to-nh-chalet-green-600',
     ]"
   >
-    <div class="flex items-center">
-      <component
-        :is="canClickHomeIcon ? 'button' : 'div'"
-        :class="[
-          'flex-shrink-0 p-1 rounded-md',
-          canClickHomeIcon ? 'cursor-pointer nh-focus-chalet-green' : 'pointer-events-none',
-        ]"
-        @click="handleIconClick"
-      >
-        <NoodleIconSvg
-          title="Noodle's House"
-          class="text-nh-bourbon w-8 h-8"
-          aria-label="Noodle's House Logo. Click to navigate Home."
-        />
-      </component>
-      <div class="hidden sm:ml-6 sm:block">
-        <div class="flex space-x-4">
-          <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <NoodleLink
-            v-for="(it, i) in dashboardStore.primaryItemList"
-            :key="`${it.id}-${i}`"
-            :to="it.to"
-            :exact-active-class="
-              it.useExactActiveClass ? 'exact-active:bg-nh-chalet-green-600' : undefined
-            "
-            :active-class="it.useActiveClass ? 'just-active:bg-nh-chalet-green-600' : undefined"
-            @click="it.click"
-            >{{ it.label }}</NoodleLink
-          >
-        </div>
-      </div>
+    <div class="flex items-center justify-left flex-1">
+      <!-- Left side Home-Icon -->
+      <SDH_HomeIcon />
+
+      <!-- Top-level nav buttons on Desktop -->
+      <SDH_D_HeaderNav />
     </div>
+
+    <!-- Middle Main title on Mobile -->
     <div
-      v-if="onNotificationsClick"
-      class="hidden sm:ml-6 sm:block"
+      v-if="USE_TITLE && $route.name"
+      class="flex sm:hidden justify-center flex-[5,5,0]"
     >
-      <div class="flex items-center">
-        <button
-          type="button"
-          :class="[
-            'relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white',
-            'nh-focus-chalet-green',
-          ]"
-          @click="$emit('notifications-click')"
-        >
-          <span class="absolute -inset-1.5" />
-          <span class="sr-only">View notifications</span>
-          <BellIcon
-            class="h-6 w-6"
-            aria-hidden="true"
-          />
-        </button>
-
-        <!-- Profile dropdown -->
-        <ProfileDropMenu :items="dashboardStore.userItemList" />
-      </div>
-    </div>
-    <div class="-mr-2 flex sm:hidden">
-      <!-- Mobile menu button -->
-      <MenuButton
+      <h3
         :class="[
-          'relative inline-flex items-center justify-center rounded-md p-2',
-          'hover:text-nh-chalet-green-200',
-          'focus:text-nh-chalet-green-200 focus:ring-2 focus:ring-nh-chalet-green-200',
-          isSidebarOpen ? 'text-nh-chalet-green-300' : 'text-nh-chalet-green-700',
+          'text-2xl font-semibold text-nh-chalet-green-800 leading-6',
+          'drop-shadow-custom shadow-nh-chalet-green-400',
         ]"
-        @click="isSidebarOpen = !isSidebarOpen"
       >
-        <span class="absolute -inset-0.5" />
-        <span class="sr-only">Open main menu</span>
+        {{ $route.name }}
+      </h3>
+    </div>
 
-        <component
-          :is="!isSidebarOpen ? Bars3Icon : XMarkIcon"
-          class="block h-6 w-6"
-          aria-hidden="true"
-        />
-      </MenuButton>
+    <div class="flex items-center justify-end flex-1">
+      <!-- Top-level right-side buttons (notifications, user profile) on Desktop -->
+      <SDH_D_RightButtons @notifications-click="$emit('notifications-click')" />
+
+      <!-- Right-side hamburger menu on Mobile -->
+      <SDH_M_Hamburger />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import NoodleIconSvg from '@/assets/NoodleIcon.svg';
-import NoodleLink from '@/components/NoodleLink.vue';
-import { RouteName } from '@/router/RouteName';
-import { useDashboardStore } from '@/stores/dashboardStore';
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/20/solid';
-import { BellIcon } from '@heroicons/vue/24/solid';
-import { storeToRefs } from 'pinia';
-import { computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import ProfileDropMenu from '../ProfileDropMenu.vue';
-import { MenuButton } from '@headlessui/vue';
+import SDH_D_HeaderNav from './SDH_D_HeaderNav.vue';
+import SDH_D_RightButtons from './SDH_D_RightButtons.vue';
+import SDH_HomeIcon from './SDH_HomeIcon.vue';
+import SDH_M_Hamburger from './SDH_M_Hamburger.vue';
 
-export interface SD_HeaderBarProps {
-  onClose?: Function;
-  onNotificationsClick?: Function;
-}
-defineProps<SD_HeaderBarProps>();
-defineEmits<{
-  close: [];
-  'notifications-click': [];
-}>();
+const USE_TITLE = true;
 
-const route = useRoute();
-const router = useRouter();
-const dashboardStore = useDashboardStore();
-const { isSidebarOpen } = storeToRefs(dashboardStore);
-
-const canClickHomeIcon = computed(() => route.name !== RouteName.HOME);
-
-function handleIconClick() {
-  // Don't duplicate navigate.
-  if (!canClickHomeIcon.value) {
-    return;
-  }
-
-  router.push({ name: RouteName.HOME });
-}
+defineProps<{ onNotificationsClick?: Function }>();
+defineEmits<{ 'notifications-click': [] }>();
 </script>

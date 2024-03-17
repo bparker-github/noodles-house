1e9343b878f876b2a822c02fd06afda6aa51cc67
bparker-github <puck2361@gmail.com>
2024-03-17 17:03:59 -0400

    Add more slots and sizing

--- ui/src/components/spinners/PageSpinner.vue
+++ ui/src/components/spinners/PageSpinner.vue
@@ -2,7 +2,7 @@
   <div class="page-spinner flex flex-col h-full justify-center items-center">
     <div
       v-if="!hideOverlay"
-      :class="['overlay', 'fixed inset-0', 'bg-nh-bourbon-950/20']"
+      :class="['overlay', 'fixed inset-0', 'bg-nh-bourbon-950/40']"
     />
     <LoadingSpinner
       :class="[
@@ -16,7 +16,24 @@
         },
       ]"
     />
-    <slot></slot>
+    <slot>
+      <div class="flex items-stretch justify-center space-y-2 mt-2">
+        <slot name="title">
+          <span
+            v-if="title"
+            class="text-lg text-nh-bourbon-950"
+            >{{ title }}</span
+          >
+        </slot>
+        <slot name="details">
+          <span
+            v-if="details"
+            class="text-lg text-nh-bourbon-950"
+            >{{ details }}</span
+          >
+        </slot>
+      </div>
+    </slot>
   </div>
 </template>
 
@@ -30,4 +47,9 @@ export interface PageSpinnerProps {
   details?: string;
 }
 defineProps<PageSpinnerProps>();
+defineSlots<{
+  default: [];
+  title: [];
+  details: [];
+}>();
 </script>

e575d342e8efdb0d684d0c97e87008f4cfa159b1
bparker-github <puck2361@gmail.com>
2024-03-18 10:30:11 -0400

    Add more logging

--- api/src/functions/infrastructure/AuthRoles.ts
+++ api/src/functions/infrastructure/AuthRoles.ts
@@ -1,21 +1,32 @@
-import { app, HttpRequest, InvocationContext } from '@azure/functions';
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 import { NoodleUserRole } from '@db/models/NoodleAuth';
 import { asSafeResponseHandler } from '../../lib/safeResponseHandler';
 
-export async function GetRoles(request: HttpRequest, context: InvocationContext) {
-  context.log(`Getting Roles for user ${JSON.stringify(request.user)}`);
+export async function GetRoles(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(
+    `Getting Roles for user ${JSON.stringify(request.user)}, body: ${JSON.stringify(request.body)}`
+  );
 
   return {
-    roles: [
-      NoodleUserRole.AUTHENTICATED,
-      NoodleUserRole.ANONYMOUS,
-      NoodleUserRole.ADMIN,
-      'quantum',
-    ],
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    jsonBody: {
+      roles: [
+        NoodleUserRole.AUTHENTICATED,
+        NoodleUserRole.ANONYMOUS,
+        NoodleUserRole.ADMIN,
+        'quantum',
+      ],
+    },
   };
 }
-app.http('Auth_GetRoles', {
-  methods: ['POST', 'GET'],
+
+app.post('Auth_GetRoles', {
   authLevel: 'function',
   route: 'auth/roles',
   handler: asSafeResponseHandler(GetRoles),

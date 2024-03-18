87bea44cd68bb60a8e1236bc09e428e32736ecad
bparker-github <puck2361@gmail.com>
2024-03-18 09:57:58 -0400

    Must test roles. Push away

--- api/src/functions/infrastructure/AuthRoles.ts
+++ api/src/functions/infrastructure/AuthRoles.ts
@@ -2,13 +2,17 @@ import { app, HttpRequest, InvocationContext } from '@azure/functions';
 import { NoodleUserRole } from '@db/models/NoodleAuth';
 import { asSafeResponseHandler } from '../../lib/safeResponseHandler';
 
-export async function GetRoles(
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<NoodleUserRole[]> {
-  context.debug(`Getting Roles for user ${request.user}`);
+export async function GetRoles(request: HttpRequest, context: InvocationContext) {
+  context.log(`Getting Roles for user ${JSON.stringify(request.user)}`);
 
-  return [NoodleUserRole.AUTHENTICATED, NoodleUserRole.ANONYMOUS];
+  return {
+    roles: [
+      NoodleUserRole.AUTHENTICATED,
+      NoodleUserRole.ANONYMOUS,
+      NoodleUserRole.ADMIN,
+      'quantum',
+    ],
+  };
 }
 app.http('Auth_GetRoles', {
   methods: ['GET'],

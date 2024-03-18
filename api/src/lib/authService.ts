8b84de950fea5925f2ec7bc591eba2079c0c0637
bparker-github <puck2361@gmail.com>
2024-03-18 09:22:02 -0400

    Add first auth service.

--- api/src/lib/authService.ts
+++ api/src/lib/authService.ts
@@ -1,3 +1,9 @@
 import { HttpRequest } from '@azure/functions';
+import { NoodleUserRole } from '@db/models/NoodleAuth.d';
+import { NoodleError } from './safeResponseHandler';
 
-export const useAuthGuard = (request: HttpRequest, forVal: string) => {};
+export const useAuthGuard = (request: HttpRequest, role: NoodleUserRole) => {
+  if (!request.user?.id) {
+    throw new NoodleError(401, 'Cannot find user');
+  }
+};

b98a66c46140f7f005771ef412e016b547f78391
bparker-github <puck2361@gmail.com>
2024-06-01 13:32:13 -0400

    Comment out task and user settings functions.

--- api/src/functions/UserSettings.ts
+++ api/src/functions/UserSettings.ts
@@ -1,29 +1,29 @@
-import { app, HttpRequest, InvocationContext } from '@azure/functions';
-import { getNoodleDb } from '../database/dataSource';
-import { UserSettings } from '../database/entity/UserSettings';
-import { NoodleError, safeResponseHandler } from '../lib/safeResponseHandler';
+// import { app, HttpRequest, InvocationContext } from '@azure/functions';
+// import { getNoodleDb } from '../database/dataSource';
+// import { UserSettings } from '../database/entity/UserSettings';
+// import { NoodleError, safeResponseHandler } from '../lib/safeResponseHandler';
 
-export async function GetUserSettings(
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<UserSettings> {
-  const userId = request.user?.id;
-  context.debug(`Getting User Settings for ${userId}`);
+// export async function GetUserSettings(
+//   request: HttpRequest,
+//   context: InvocationContext
+// ): Promise<UserSettings> {
+//   const userId = request.user?.id;
+//   context.debug(`Getting User Settings for ${userId}`);
 
-  if (!userId) {
-    context.warn('Attempting to get settings without user');
-    throw new NoodleError(401, 'Cannot get settings without user.');
-  }
+//   if (!userId) {
+//     context.warn('Attempting to get settings without user');
+//     throw new NoodleError(401, 'Cannot get settings without user.');
+//   }
 
-  const noodleDb = await getNoodleDb();
-  const found = await noodleDb.manager.findOneBy(UserSettings, { id: userId });
+//   const noodleDb = await getNoodleDb();
+//   const found = await noodleDb.manager.findOneBy(UserSettings, { id: userId });
 
-  context.debug(`Returning ${!found ? 'empty' : 'found'} User Settings for ${userId}`);
-  return found ?? UserSettings.Default(userId);
-}
-app.http('UserSettings_GetMy', {
-  methods: ['GET'],
-  authLevel: 'function',
-  route: 'settings',
-  handler: (r, c) => safeResponseHandler(r, c, GetUserSettings),
-});
+//   context.debug(`Returning ${!found ? 'empty' : 'found'} User Settings for ${userId}`);
+//   return found ?? UserSettings.Default(userId);
+// }
+// app.http('UserSettings_GetMy', {
+//   methods: ['GET'],
+//   authLevel: 'function',
+//   route: 'settings',
+//   handler: (r, c) => safeResponseHandler(r, c, GetUserSettings),
+// });
--- api/src/functions/tasks/getMyTasks.ts
+++ api/src/functions/tasks/getMyTasks.ts
@@ -1,26 +1,26 @@
-import { app, HttpRequest, InvocationContext } from '@azure/functions';
-import { getNoodleDb } from '../../database/dataSource';
-import { TodoTaskModel } from '../../database/entity/TodoTask';
-import { NoodleError, safeResponseHandler } from '../../lib/safeResponseHandler';
+// import { app, HttpRequest, InvocationContext } from '@azure/functions';
+// import { getNoodleDb } from '../../database/dataSource';
+// import { TodoTaskModel } from '../../database/entity/TodoTask';
+// import { NoodleError, safeResponseHandler } from '../../lib/safeResponseHandler';
 
-export async function GetMyTasks(
-  request: HttpRequest,
-  context: InvocationContext
-): Promise<TodoTaskModel[]> {
-  context.debug(`Getting Tasks for specific user`);
-  const noodleDb = await getNoodleDb();
+// export async function GetMyTasks(
+//   request: HttpRequest,
+//   context: InvocationContext
+// ): Promise<TodoTaskModel[]> {
+//   context.debug(`Getting Tasks for specific user`);
+//   const noodleDb = await getNoodleDb();
 
-  const userId = request.user?.id;
-  if (!userId) {
-    throw new NoodleError(401, 'Cannot get tasks without user.');
-  }
+//   const userId = request.user?.id;
+//   if (!userId) {
+//     throw new NoodleError(401, 'Cannot get tasks without user.');
+//   }
 
-  return await noodleDb.manager.findBy(TodoTaskModel, { createdBy: userId });
-}
+//   return await noodleDb.manager.findBy(TodoTaskModel, { createdBy: userId });
+// }
 
-app.http('Tasks_GetMy', {
-  methods: ['GET'],
-  authLevel: 'function',
-  route: 'tasks',
-  handler: (r, c) => safeResponseHandler(r, c, GetMyTasks),
-});
+// app.http('Tasks_GetMy', {
+//   methods: ['GET'],
+//   authLevel: 'function',
+//   route: 'tasks',
+//   handler: (r, c) => safeResponseHandler(r, c, GetMyTasks),
+// });

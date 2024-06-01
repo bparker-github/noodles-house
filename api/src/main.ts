6a28d1732cec2d8a24c8713d24505670953dee30
bparker-github <puck2361@gmail.com>
2024-06-01 14:03:35 -0400

    Add functions to the main

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,15 @@ export * from './database/dataSource';
 export * from './database/entity/TodoTask';
 export * from './database/entity/UserSettings';
 
-export * from './generated/swa-db-connections/src/index';
+export * from './functions/Unsplash';
+export * from './functions/UserSettings';
+export * from './functions/infrastructure/AuthRoles';
+export * from './functions/infrastructure/DatabaseInit';
+export * from './functions/infrastructure/DatabaseSync';
+export * from './functions/tasks/createNewTask';
+export * from './functions/tasks/editTask';
+export * from './functions/tasks/getMyTasks';
+export * from './functions/tasks/getMyTasksCount';
 
 export * from './lib/authService';
 export * from './lib/safeResponseHandler';

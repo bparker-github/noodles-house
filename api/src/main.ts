08051dd0b12d89f58bf4fc07fd24fb16ca0c9b9c
bparker-github <puck2361@gmail.com>
2024-06-01 13:50:02 -0400

    Add main file to reference all known API things.

---
+++ api/src/main.ts
@@ -0,0 +1,8 @@
+export * from './database/dataSource';
+export * from './database/entity/TodoTask';
+export * from './database/entity/UserSettings';
+
+export * from './generated/swa-db-connections/src/index';
+
+export * from './lib/authService';
+export * from './lib/safeResponseHandler';

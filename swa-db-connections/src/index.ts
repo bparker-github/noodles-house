f42a188a75d07361220ecaf771773dc833344ec4
bparker-github <puck2361@gmail.com>
2024-06-07 10:23:23 -0400

    Delete package files from swa-db-connections

--- swa-db-connections/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './models/ModelResponse';
-export * from './models/NoodleAuth';
-export * from './models/TodoTask';
-export * from './models/UserSettings';
--- swa-db-connections/src/models/NoodleAuth.ts
+++
@@ -1,6 +0,0 @@
-export enum NoodleUserRole {
-  ANONYMOUS = 'anonymous',
-  AUTHENTICATED = 'authenticated',
-  ADMIN = 'admin',
-  LORD_OF_THE_HOUSE = 'lord-of-the-house',
-}

d8aa7ec31f1b3b06ecd9a9e3860d32666405e69b
bparker-github <puck2361@gmail.com>
2024-06-15 12:40:24 -0400

    Export all functions.

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,14 +5,14 @@ export * from './database/entity/UserSettings';
 
 // Functions references
 export * from './functions/Unsplash';
-// export * from './functions/UserSettings';
+export * from './functions/UserSettings';
 export * from './functions/infrastructure/AuthRoles';
 export * from './functions/infrastructure/DatabaseInit';
 export * from './functions/infrastructure/DatabaseSync';
-// export * from './functions/tasks/createNewTask';
-// export * from './functions/tasks/editTask';
-// export * from './functions/tasks/getMyTasks';
-// export * from './functions/tasks/getMyTasksCount';
+export * from './functions/tasks/createNewTask';
+export * from './functions/tasks/editTask';
+export * from './functions/tasks/getMyTasks';
+export * from './functions/tasks/getMyTasksCount';
 
 // Library references
 export * from './lib/authService';

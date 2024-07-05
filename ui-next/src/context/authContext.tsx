c9112e4f4968d323e8bc73715ae861e40e5d338c
bparker-github <puck2361@gmail.com>
2024-07-05 12:56:33 -0400

    Update Auth context a bit.

--- ui-next/src/context/authContext.tsx
+++ ui-next/src/context/authContext.tsx
@@ -1,33 +1,58 @@
 'use client';
 
-import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
+import {
+  PropsWithChildren,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 export interface AuthContextState {
+  isLoggedIn: boolean;
+  isLoggingIn: boolean;
   user: unknown;
-  setUser: unknown;
 }
 
-export const AuthContext = createContext<AuthContextState | null>(null);
-
+export const AuthContext = createContext<AuthContextState>({
+  isLoggedIn: false,
+  isLoggingIn: false,
+  user: null,
+});
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [user, setUser] = useState<{} | null>(null);
+  const isLoggedIn = !!user;
+
+  const checkCurrentAuth = useCallback(async () => {
+    if (isLoggedIn || isLoggingIn) return;
+
+    setIsLoggingIn(true);
+
+    try {
+      const authResult = await fetch('/.auth/me', { next: { revalidate: 300 } });
+
+      if (authResult.ok) {
+        setUser(authResult.json());
+      } else {
+        console.error('Failed to fetch meeee:', authResult.json());
+        setUser(null);
+      }
+    } catch (err) {
+      console.error('Bigger error:', err);
+    }
+  }, [isLoggedIn, isLoggingIn]);
 
   useEffect(() => {
-    fetch('/.auth/me', { next: { revalidate: 300 } })
-      .then((authResult) => {
-        if (authResult.ok) {
-          setUser(authResult.json());
-        } else {
-          console.error('Failed to fetch meeee:', authResult.json());
-          setUser(null);
-        }
-      })
-      .catch((err) => {
-        console.error('Bigger error:', err);
-      });
-  }, []);
+    checkCurrentAuth();
+  }, [checkCurrentAuth]);
 
-  return <AuthContext.Provider value={{ setUser, user }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoggingIn, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };

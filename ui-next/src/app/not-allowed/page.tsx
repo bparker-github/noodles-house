3145e2b2d14d6201116cf7ead94b36810336ea6c
bparker-github <puck2361@gmail.com>
2024-07-05 12:56:44 -0400

    Add wrapper for the unsplash around the page, import back into the not-allowed page.

--- ui-next/src/app/not-allowed/page.tsx
+++ ui-next/src/app/not-allowed/page.tsx
@@ -1,9 +1,9 @@
-// import { UnsplashImage } from '@noodles-house/unsplash';
+import { UnsplashImage } from '@noodles-house/unsplash';
 
 export default function NotAllowedPage() {
   return (
     <main className="relative isolate min-h-full">
-      <div
+      <UnsplashImage
         id="W7cPLHOa0eQ"
         className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
         shared-class="flex flex-col flex-1 items-center justify-center rounded-md"
--- ui-next/src/app/template.tsx
+++ ui-next/src/app/template.tsx
@@ -1,6 +1,20 @@
 import { AuthProvider } from '@/context/authContext';
 import { PropsWithChildren } from 'react';
+import { UnsplashProvider } from '@noodles-house/unsplash';
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
-  return <AuthProvider>{children}</AuthProvider>;
+  const unsplashAccessKey = process.env.NOOD_UNSPLASH_ACCESS_KEY ?? '';
+  const uiEndpoint =
+    typeof window === 'undefined' ? '' : window?.location?.origin + '/api/unsplash';
+
+  return (
+    <AuthProvider>
+      <UnsplashProvider
+        uiEndpoint={uiEndpoint}
+        apiKey={unsplashAccessKey}
+      >
+        {children}
+      </UnsplashProvider>
+    </AuthProvider>
+  );
 }
